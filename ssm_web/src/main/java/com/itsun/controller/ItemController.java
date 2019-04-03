5bfba310c9448d9281218be390cad2d6827c1a56
admin <heimaadmin@163.com>
2019-04-03 18:58:00 +0800

    冲突

--- ssm_web/src/main/java/com/itsun/controller/ItemController.java
+++ ssm_web/src/main/java/com/itsun/controller/ItemController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ItemController {
     @Autowired
     private ItemService itemService;
-
+//造成冲突
     @RequestMapping("/findDetail")
     public String findAll(Model model){
         Items items = itemService.findById(1);
